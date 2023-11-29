53d2c2768549d51474f670ad6894db4a4d0a888f
RogShotz <lukeroweprivate@gmail.com>
2023-11-28 17:01:56 -0800

    add base commit, p0001, and tester

---
+++ solutions/p_0001.py
@@ -0,0 +1,27 @@
+# 1. Two Sum
+#
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+#
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#
+# You can return the answer in any order.
+
+# Constraints:
+#
+# 2 <= nums.length <= 104
+# -109 <= nums[i] <= 109
+# -109 <= target <= 109
+# Only one valid answer exists.
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return list([i, j])
---
+++ tester.py
@@ -0,0 +1,9 @@
+from solutions import p_0001 as sol
+
+ob = sol.Solution()
+
+result = ob.two_sum([2,7,11,15], 9)
+print(result)
+result = ob.two_sum([3,2,4], 6)
+print(result)
+
